2eb88623091ce7c2bdb6d4ad235ed7754b25a6e2
Anatoliy Sablin <sablintolya@gmail.com>
2018-06-15 22:29:33 +0300

    Matrix identity server (POC).

---
+++ src/test/kotlin/io/github/ma1uta/identity/StatusTest.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright sablintolya@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.ma1uta.identity
+
+import org.junit.Before
+import org.junit.Ignore
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Profile
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(classes = [Identity::class])
+@Profile("test")
+class StatusTest {
+
+    lateinit var mockMvc: MockMvc
+
+    @Autowired
+    lateinit var webApplicationContext: WebApplicationContext
+
+    @Before
+    fun init() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build()
+    }
+
+    @Ignore
+    @Test
+    fun statusTest() {
+        mockMvc.perform(get("/_matrix/identity/api/v1")).andExpect(status().isOk)
+    }
+}
